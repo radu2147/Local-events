5df27fa038c7ab7bfb0532bfca5965a0f7e3a221
unknown <radubaston1@gmail.com>
2020-12-28 23:33:55 +0200

    Created the database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const port = 8080;
 
 const commRouter = require("./comm/router");
+const {Sequelize} = require('sequelize');
+const init = require('./relationInit');
 
 const app = express();
 
-const func = (req, res, next) => {
-    console.log("Comm");
-    next();
-}
-
 app.use(cors());
-app.get('/test', func, (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.get('/test', (req, res) => {
     res.status(200).send({message: "Hello World!"});
 })
 
-app.use('/api', func, commRouter);
+app.use('/api', commRouter);
+
+
+init();
 
 module.exports = [app, port];
--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,16 +1,52 @@
-import React, { cloneElement, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import axios from 'axios';
 
 const App = () => {
     const [message, setMessage] = useState("Test");
+    const [name, setName] = useState("");
+    const [text, setText] = useState(""); 
     useEffect(() => {
-        fetch("http://localhost:8080/api")
+        fetch("http://localhost:8080/test")
         .then(e => e.json())
         .then(e => {
             setMessage(e.message)
         })
 
     }, [])
-    return <p>{message}</p>
+    const postFunc = (e) => {
+        e.preventDefault();
+        const headers = {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*'
+        }
+        axios.post('http://localhost:8080/api/add-comms',{
+               method: "POST",
+               headers: headers,
+               username: name,
+               text: text
+        }
+        )
+        .then(e => console.log(e))
+            
+    }
+
+    const deleteInstance = (e) => {
+        e.preventDefault();
+        axios.delete('http://localhost:8080/api/delete/2')
+            .then(e => console.log(e));
+    }
+
+    return (
+        <div>
+            <p>{message}</p>
+            <form onSubmit={postFunc}>
+                <input name="commName" type="text" placeholder="Nume" onChange={e => {setName(e.target.value);}}/>
+                <input name="commText" type="text" placeholder="Text" onChange={e => setText(e.target.value)}/>
+                <button type="submit" value="submit">Buna</button>
+            </form>
+            <button value="submit" onClick={deleteInstance}>Sterge</button>
+        </div>
+    )
 }
 ReactDOM.render(<App/>, document.getElementById('root'));
