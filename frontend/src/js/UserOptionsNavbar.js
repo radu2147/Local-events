4e05f5160e86e395f36b16c4ec727fb4338bba97
RADU <radubaston1@gmail.com>
2021-02-25 15:49:48 +0200

    Added button to add event

--- frontend/src/js/UserOptionsNavbar.js
+++ frontend/src/js/UserOptionsNavbar.js
@@ -14,6 +14,10 @@ const UserOptionsNavbar = () => {
                     Evenimentele mele
                 </li>
             </a>
+
+            <a className="auth-btn add-event" href="/profile/add-event">
+                Adauga evenimente
+            </a>
         </div>
     )
 }
